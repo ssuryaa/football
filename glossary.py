b2e0252e9ca084bd010aeaf8662b70d7ddea50ea
ssuryaa <sathiyasuryanarayanan@gmail.com>
2020-01-20 20:35:20 -0700

    a

---
+++ glossary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def get_glossary():
+    glossary = {
+        'Ast' : 'Assist',
+        'xA' : 'Expected Assists',
+        'A-xA' : 'Assists minum Expected Assists',
+        'KP' : 'Key Passes',
+        'TB' : 'Through Balls',
+        '1/3' : 'Passes into Final Third',
+        'PPA' : 'Passes into Penalty Area',
+        'CrsPA' : 'Crosses into Penalty Area'        
+    }
+
+    return glossary
---
+++ percentile.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def calc_percentile(stats):
+    

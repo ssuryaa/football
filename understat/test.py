34f981d96774a83060bfe25c3d903f712b34f2db
ssuryaa <sathiyasuryanarayanan@gmail.com>
2020-07-25 21:31:38 -0600

    Added dumps

--- understat/test.py
+++ understat/test.py
@@ -7,12 +7,22 @@ import aiohttp
 
 from understat import Understat
 
-async def main():
+'''
+async def get_players():
 	async with aiohttp.ClientSession() as session:
 		understat = Understat(session)
-		player = await understat.get_league_players("epl", 2018, player_name="Paul Pogba", team_title="Manchester United")
-		print(json.dumps(player))
+		player = await understat.get_league_players("epl", 2018, player_name="Marcus Rashford", team_title="Manchester United")
+		#print(json.dumps(player))
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-			
+loop.run_until_complete(get_players())
+'''
+
+async def get_shots():
+	async with aiohttp.ClientSession() as session:
+		understat = Understat(session)
+		player_shots = await understat.get_player_shots(556)
+		print(json.dumps(player_shots))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(get_shots())
